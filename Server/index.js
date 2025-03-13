25ba3d1fd50f2ad31cbc7f65d9212d2ccede8889
Rishi Sharma <rishisharma@Rishis-MacBook-Air.local>
2025-03-13 22:16:51 +0530

    updated index

--- Server/index.js
+++ Server/index.js
@@ -23,12 +23,23 @@ await dbConnect();
 
 app.use(express.json());
 app.use(cookieParser())
-app.use(cors(
-    {
-        origin: "http://localhost:5173",
+const allowedOrigins = [
+    "http://localhost:5173",
+    "https://studi-fy.vercel.app"
+];
+
+app.use(
+    cors({
+        origin: function (origin, callback) {
+            if (!origin || allowedOrigins.includes(origin)) {
+                callback(null, true);
+            } else {
+                callback(new Error("Not allowed by CORS"));
+            }
+        },
         credentials: true,
-    }
-))
+    })
+);
 // app.use(fileUpload({
 //     useTempFiles:true,
 //     tempFileDir:"/temp"
