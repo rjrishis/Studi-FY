df2690aac31835a856b4ae451e2ceaa3e076b831
rjrishis <95604505+rjrishis@users.noreply.github.com>
2025-03-15 16:53:37 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:4000/api/v1"
+const BASE_URL = "https://studi-fy.onrender.com/api/v1"
 
 export const categories = {
   CATEGORIES_API: BASE_URL + "/course/showAllCategories"
